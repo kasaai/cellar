93964b2b3fc2d6dbd88762f752b68c7c6d486800
kevinykuo <kevinykuo@gmail.com>
2019-12-07 22:41:18 -0800

    fix board_register_datatxt() call

--- r/R/cellar.R
+++ r/R/cellar.R
@@ -3,6 +3,6 @@
 #' @param dataset Name of the dataset.
 #' @export
 cellar_pull <- function(dataset) {
-  pins::board_register_datatxt("cellar", "https://cellar.kasa.ai")
+  pins::board_register_datatxt("https://cellar.kasa.ai", name = "cellar")
   pins::pin_get(dataset, board = "cellar")
 }
