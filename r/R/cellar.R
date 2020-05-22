08cb93185636e83ead9f6709e691b9d5dad73edc
kevinykuo <kevinykuo@gmail.com>
2020-05-22 17:37:31 +0800

    use S3 board

--- r/R/cellar.R
+++ r/R/cellar.R
@@ -3,6 +3,6 @@
 #' @param dataset Name of the dataset.
 #' @export
 cellar_pull <- function(dataset) {
-  pins::board_register_datatxt("https://cellar.kasa.ai", name = "cellar")
+  pins::board_register_datatxt("http://data.cellar.kasa.ai", name = "cellar")
   pins::pin_get(dataset, board = "cellar")
 }
